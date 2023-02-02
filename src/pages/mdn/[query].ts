c024803cf1b48fa8157c96a01142dd69b2662c68
Fynn Path <pathfynn@gmail.com>
2023-02-02 17:07:05 +0100

    Remove lang from MDN redirect

--- src/pages/mdn/[query].ts
+++ src/pages/mdn/[query].ts
@@ -1,7 +1,5 @@
 import type { APIRoute } from "astro";
 
 export const get: APIRoute = async function get({ params, redirect }) {
-  return redirect(
-    `https://developer.mozilla.org/en-US/search?q=${params.query}`
-  );
+  return redirect(`https://developer.mozilla.org/search?q=${params.query}`);
 };
