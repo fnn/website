42adacf7f5b4a2bbd33ec69b550a6330d03ca293
Fynn Path <pathfynn@gmail.com>
2023-02-02 16:10:58 +0100

    Enable server rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [solid(), tailwind()],
+  adapter: cloudflare()
 });
