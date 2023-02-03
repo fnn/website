0b2d08192600311e17d290c7cd8907180fc31e67
Fynn Path <pathfynn@gmail.com>
2023-02-03 19:08:23 +0100

    Add MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [solid(), tailwind()],
+  integrations: [solid(), tailwind(), mdx()],
   adapter: cloudflare()
 });
