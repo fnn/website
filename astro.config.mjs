a7c107367f62bd25b66a529881a9d9327963bfa0
Fynn Path <pathfynn@gmail.com>
2023-03-16 23:10:43 +0100

    build: switch to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
-
 import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [solid(), tailwind(), mdx()],
-  adapter: cloudflare(),
+  adapter: vercel({
+    analytics: true,
+  }),
   vite: {
     ssr: {
       noExternal: ["@kobalte/core"],
